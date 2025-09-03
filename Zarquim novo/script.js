6b0cb28a4c47bcacbf6075ce70d687491a6b90b0
Isac Castro <53198685+toddynic@users.noreply.github.com>
2025-09-03 11:29:58 -0300

    Add files via upload
    TRABALHO

---
+++ Zarquim novo/script.js
@@ -0,0 +1,16 @@
+let DB = true;
+
+let plan = [
+    {location: 'Salvador - BA', img : "...",  description: 'Uma cidade linda na Bahia.'},
+    {location: 'Natal - RN', img : "...",  description: 'Famosa por suas praias.'},
+    {location: 'Santiago - Chile', img : "...",  description: 'A capital do Chile, cercada por montanhas.'},
+    {location: 'Buenos Aires - Argentina', img : "...",  description: 'Conhecida como a Paris da América do Sul.'}
+]
+
+function click(location) {
+    if (DB === false) {return;}
+
+
+
+}
+
